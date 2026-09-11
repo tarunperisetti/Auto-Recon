20e00786118957aa9e715e4c41301bfcf4f62fa9
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-11 18:34:57 +0530

    bash and python scripts are added

--- recon/domain_recon.py
+++ recon/domain_recon.py
@@ -2,3 +2,4 @@ from core.runner import run_bash
 
 def domain_recon(target):
     print(run_bash("modules/bash/ping.sh",target))
+    print(run_bash("modules/bash/whois.sh",target))
--- recon/ip_recon.py
+++ recon/ip_recon.py
@@ -2,7 +2,7 @@ from core.runner import run_bash
 from modules.python.geoip import run_geoip
 
 def ip_recon(target):
-    print(run_bash("modules/bash/whois.sh",target))
     print(run_bash("modules/bash/ping.sh",target))
+    print(run_bash("modules/bash/whois.sh",target))
 
     run_geoip(target)
