7c8721736cdc3a2c3db9505d5c90aa4f86c1f362
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-11 18:36:50 +0530

    sub process

--- core/runner.py
+++ core/runner.py
@@ -3,9 +3,12 @@ import subprocess
 def run_bash(script,target):
     result = subprocess.run(["bash",script,target],capture_output=True,text=True)
 
+    print("Return code:", result.returncode)
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+     
     if result.returncode != 0:
         print(f"[!] Error running {script}")
-        print(result.stderr)
         return None
 
     return result.stdout.strip()
