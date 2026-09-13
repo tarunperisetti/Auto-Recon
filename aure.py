3b81818bd3b6b24c18f0c69b9c8982e7e15a8e47
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-13 15:14:31 +0530

    banner added

--- aure.py
+++ aure.py
@@ -1,10 +1,20 @@
 from core.valid import classify_target
 from recon.ip_recon import ip_recon
 from recon.domain_recon import domain_recon
+import pyfiglet 
+from colorama import Fore,init
+
+init(autoreset=True)
+def banner():
+    print(Fore.RED+"="*79)
+    print(Fore.RED+pyfiglet.figlet_format("AUTORECON", font="ansi_shadow"))
+    print(Fore.RED+"               Basic automatic passive and active reconnassince")
+    print(Fore.RED+"="*79)
 
 #-----------------main-----------------
 
-target = input("[+] Enter IP address or Domain name : ")
+banner()
+target = input("\n[+] Enter IP address or Domain name : ")
 target_type = classify_target(target)
 if target_type == "IP":
     ip_recon(target)
