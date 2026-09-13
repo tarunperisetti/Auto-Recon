422f454df36e8392d7f40ade1d86efeff02ae714
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-13 15:15:26 +0530

    changes in runner.py

--- core/runner.py
+++ core/runner.py
@@ -2,13 +2,13 @@ import subprocess
 
 def run_bash(script,target):
     result = subprocess.run(["bash",script,target],capture_output=True,text=True)
-
-    print("Return code:", result.returncode)
-    print("STDOUT:", result.stdout)
-    print("STDERR:", result.stderr)
-     
+   
     if result.returncode != 0:
         print(f"[!] Error running {script}")
+
+        if result.stderr.strip():
+            print(result.stderr.strip())
+
         return None
 
     return result.stdout.strip()
