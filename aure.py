4dea0e5bb9e0855420ca046f159c37ce0870c7a6
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-14 19:24:45 +0530

    added ip resolve by domain

--- aure.py
+++ aure.py
@@ -1,6 +1,7 @@
 from core.valid import classify_target
 from recon.ip_recon import ip_recon
 from recon.domain_recon import domain_recon
+import socket
 import pyfiglet 
 from colorama import Fore,init
 
@@ -11,11 +12,19 @@ def banner():
     print(Fore.RED+"               Basic automatic passive and active reconnassince")
     print(Fore.RED+"="*79)
 
+def resolve(target):
+    return socket.gethostbyname(target)
+
 #-----------------main-----------------
 
 banner()
 target = input("\n[+] Enter IP address or Domain name : ")
 target_type = classify_target(target)
+
+if target_type == "DOMAIN":
+    ip = resolve(target)
+    print(f"[+] Target ip : {ip}")
+
 if target_type == "IP":
     ip_recon(target)
 elif target_type == "DOMAIN":
