20e5ea1ad3d0c7c12b761bcce95c963b94ff58e5
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-15 22:30:52 +0530

    linked dns_enum.sh script

--- recon/domain_recon.py
+++ recon/domain_recon.py
@@ -3,3 +3,4 @@ from core.runner import run_bash
 def domain_recon(target):
     print(run_bash("modules/bash/ping.sh",target))
     print(run_bash("modules/bash/whois.sh",target))
+    print(run_bash("modules/bash/dns_enum.sh",target))
