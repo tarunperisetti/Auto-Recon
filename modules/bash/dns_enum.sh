53eb6771539a19f05afd7f6b739f9210c546d52d
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-16 19:46:47 +0530

    added ip based dns enumeration

--- modules/bash/dns_enum.sh
+++ modules/bash/dns_enum.sh
@@ -6,13 +6,20 @@ if [ -z "$target" ]; then
     exit 1
 fi
 
-echo "[+] Target : $target"
-echo
-echo "----------------NS RECORDS-----------------"
-dig "$target" NS +short
-echo
-echo "----------------MX RECORD------------------"
-dig "$target" MX +short
-echo
-echo "----------------TXT RECORD-----------------"
-dig "$target" TXT +short
+if [[ "$target" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "[+] Target : $target"
+    echo
+    echo "-----------------PTR / REVERSE DNS-----------------"
+    dig -x "$target" +short
+else
+    echo "[+] Target : $target"
+    echo
+    echo "----------------NS RECORDS-----------------"
+    dig "$target" NS +short
+    echo
+    echo "----------------MX RECORD------------------"
+    dig "$target" MX +short
+    echo
+    echo "----------------TXT RECORD-----------------"
+    dig "$target" TXT +short
+fi
