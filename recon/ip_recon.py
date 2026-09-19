931c4a69c6efd65866b20e018ac56c1d66a257c0
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-19 21:01:19 +0530

    traceroute script added

--- recon/ip_recon.py
+++ recon/ip_recon.py
@@ -5,5 +5,6 @@ def ip_recon(target):
     print(run_bash("modules/bash/ping.sh",target))
     print(run_bash("modules/bash/whois.sh",target))
     print(run_bash("modules/bash/dns_enum.sh",target))
+    print(run_bash("modules/bash/traceroute.sh",target))
 
     run_geoip(target)
