df010721c4bbc83e5ce692d9f2d71c5352910cab
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-19 21:32:10 +0530

    sidebanner

--- recon/ip_recon.py
+++ recon/ip_recon.py
@@ -1,10 +1,21 @@
 from core.runner import run_bash
 from modules.python.geoip import run_geoip
+from colorama import Fore,init
+
+init(autoreset=True)
+def sidebanner(n):
+    print(Fore.CYAN+"="*79)
+    print(Fore.RED+n)
+    print(Fore.CYAN+"="*79)
 
 def ip_recon(target):
+    sidebanner("[-------------------------------------PING--------------------------------------]")
     print(run_bash("modules/bash/ping.sh",target))
+    sidebanner("[-------------------------------------WHOIS-------------------------------------]")
     print(run_bash("modules/bash/whois.sh",target))
+    sidebanner("[--------------------------------DNS ENUMERATION--------------------------------]")
     print(run_bash("modules/bash/dns_enum.sh",target))
+    sidebanner("[----------------------------------TRACEROUTE-----------------------------------]")
     print(run_bash("modules/bash/traceroute.sh",target))
-
+    sidebanner("[------------------------------------GEOIP--------------------------------------]")
     run_geoip(target)
