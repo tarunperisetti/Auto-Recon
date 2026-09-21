9fbde12e5f4b8375d9ba1647154c745c21e996d8
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-21 17:02:05 +0530

    nmap scripts linked

--- recon/domain_recon.py
+++ recon/domain_recon.py
@@ -8,13 +8,13 @@ def sidebanner(n):
     print(Fore.CYAN+"="*79)
 
 def domain_recon(target):
-    sidebanner("[------------------------------------PING-------------------------------------]")
+    sidebanner("[-------------------------------------PING--------------------------------------]")
     print(run_bash("modules/bash/ping.sh",target))
-    sidebanner("[------------------------------------WHOIS------------------------------------]")
+    sidebanner("[-------------------------------------WHOIS-------------------------------------]")
     print(run_bash("modules/bash/whois.sh",target))
-    sidebanner("[-------------------------------DNS ENUMERATION-------------------------------]")
+    sidebanner("[--------------------------------DNS ENUMERATION--------------------------------]")
     print(run_bash("modules/bash/dns_enum.sh",target))
-    sidebanner("[---------------------------------TRACEROUTE----------------------------------]")
+    sidebanner("[----------------------------------TRACEROUTE-----------------------------------]")
     print(run_bash("modules/bash/traceroute.sh",target))
     sidebanner("[--------------------------------NMAP BASIC SCAN--------------------------------]")
     print(run_bash("modules/bash/nmap_basic.sh",target))
