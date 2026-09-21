23f9e7201ba0f943720c8b96d139842c92ec4547
Tarun Perisetti <suryaxbhai19@gmail.com>
2026-09-21 16:58:03 +0530

    modified geoip

--- modules/python/geoip.py
+++ modules/python/geoip.py
@@ -1,9 +1,6 @@
 import requests
 
 def run_geoip(ip):
-    print("\n"+"=" * 45)
-    print("[+] GEO IP")
-    print("=" * 45)
     
     url = f"http://ip-api.com/json/{ip}"
     response = requests.get(url, timeout=5).json()
